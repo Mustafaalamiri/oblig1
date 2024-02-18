e4ef7495758420c319dffc45c03928d840126dc4
Mustafa <s374984@oslomet.no>
2024-02-18 22:35:28 +0100

    Legger til billettliste, trekker inn input til JavaScript og lager array

---
+++ src/main/resources/static/Bestilling.js
@@ -0,0 +1,10 @@
+let billett = [];
+
+function kjopBillet(){
+    const fornavn= document.getElementById("fornavn".value);
+    const etternavn= document.getElementById("etternavn".value);
+    const telefon= document.getElementById("telefon".value);
+    const epost= document.getElementById("epost".value);
+    const antall = parseInt(document.getElementById("antall").value);
+    const film = document.getElementById("film".value);
+}
