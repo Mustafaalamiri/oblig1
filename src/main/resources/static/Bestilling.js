6c784ecb46b17d4874254eb75962a6efe967f6c9
Mustafa <s374984@oslomet.no>
2024-02-18 23:17:30 +0100

    Legger til span for validering

--- src/main/resources/static/Bestilling.js
+++ src/main/resources/static/Bestilling.js
@@ -8,4 +8,30 @@ function kjopBillet(){
     const antall = parseInt(document.getElementById("antall").value);
     const film = document.getElementById("film".value);
 
+    // bruker .textContent for å erstatte/redigere tekst i span-elementene
+    document.getElementById("filmError").textContent = "";
+    document.getElementById("antallError").textContent = "";
+    document.getElementById("fornavnError").textContent = "";
+    document.getElementById("etternavnError").textContent = "";
+    document.getElementById("telefonError").textContent = "";
+    document.getElementById("epostError").textContent = "";
+    if(!fornavn || !etternavn || !telefon || !epost || !film || !antall || isNaN(antall)){
+        if(!film){
+            document.getElementById("filmError").textContent = "Må velge film";
+        }
+        if(!antall || isNaN(antall)) {
+            document.getElementById("antallError").textContent = "Må skrive noe inn i antall";
+        }
+        if(!fornavn){
+            document.getElementById("fornavnError").textContent = "Må skrive noe inn i fornavn";
+        }
+        if(!etternavn){
+            document.getElementById("etternavnError").textContent = "Må skrive noe inn i etternavn";
+        }
+        if(!telefon){
+            document.getElementById("telefonError").textContent = "Må skrive noe inn i telefon";
+        }
+        if(!epost){
+            document.getElementById("epostError").textContent = "Må skrive noe inn i epost";
+        }
 }
